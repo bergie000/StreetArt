dccc5f7a9aec49da993c80e32506d5dcb7b93d65
reeroo00 <lisabergin87@gmail.com>
2018-03-03 23:01:54 +0000

    added comment headers to files

--- js/streetart.js
+++ js/streetart.js
@@ -1,9 +1,20 @@
+// * Author: Lisa Bergin
+
+// * Assignment: Minor Project – Web Application Build
+
+// * Student ID: STU-00001445
+
+// * Date: 2018/03/03
+
+
+// enables bootstrap tooltip
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
 })
 
 $(document).ready(function(){
 
+// hover on username to affect both profile pic and username
 	$('.headerright').mouseover(function () {
 		$('.username').css('text-decoration', 'underline');
 	})
@@ -11,7 +22,7 @@ $(document).ready(function(){
 		$('.username').css('text-decoration', 'none');
 	});
 
-
+// changes photo upload section to say name of file once file is added
 	$("#uploadphoto").change(function () {
 		var uploadphoto = $('#uploadphoto').val();
 		var backslash = uploadphoto.lastIndexOf("\\") + 1;
